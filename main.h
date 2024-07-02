2832d3cb518e3ec980f7f347c274fa4c938d5a84
CloudSlinger <tom.tang@g.austincc.edu>
2024-07-02 15:32:51 -0500

    Added directives for input and output.

--- main.h
+++ main.h
@@ -5,8 +5,10 @@ Purpose of the file: This is a header file that contains directives
 needed for main.cpp.
 ***************************/
 
-
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <iostream>     /* cout, endl */
+#include <stdlib.h>     /* srand, rand */
 
 #endif //MAIN_H
