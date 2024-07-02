7c9ab87372595194b38352094e8251822e5a313c
CloudSlinger <tom.tang@g.austincc.edu>
2024-07-02 15:31:18 -0500

    Updated header guard.

--- main.h
+++ main.h
@@ -1,12 +1,12 @@
 /**************************
 Name: Tom Tang
-Assignment: Final Project: Classes and Objects
+Assignment: Final Project
 Purpose of the file: This is a header file that contains directives
 needed for main.cpp.
 ***************************/
 
 
-#ifndef FINAL_PROJECT_CLOUDSLINGER_MAIN_H
-#define FINAL_PROJECT_CLOUDSLINGER_MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#endif //FINAL_PROJECT_CLOUDSLINGER_MAIN_H
+#endif //MAIN_H
