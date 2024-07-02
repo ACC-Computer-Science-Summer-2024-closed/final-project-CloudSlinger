35fc647f43d1e09a613b9f3be8d44480ccff76ba
CloudSlinger <tom.tang@g.austincc.edu>
2024-07-02 15:29:59 -0500

    Added main() function.

--- main.cpp
+++ main.cpp
@@ -4,3 +4,10 @@ Assignment: Final Project
 Purpose of the file: This is the main source file that includes
 a function to print a message to the console.
 ***************************/
+
+#include "main.h"
+
+int main() {
+
+    return 0;
+}
