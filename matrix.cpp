9f2c0e6d051887237b0a48e52c3a32634489149f
CloudSlinger <tom.tang@g.austincc.edu>
2024-07-03 18:12:15 -0500

    Added overload constructor for Matrix().

--- matrix.cpp
+++ matrix.cpp
@@ -5,3 +5,67 @@ Purpose of the file: This file implements the functions declared in matrix.h.
 ***************************/
 
 #include "matrix.h"
+
+
+Matrix::Matrix(int rows, int cols) {
+
+    /*********************************************
+    Constructor to initialize the Matrix with given
+    rows and columns, and an optional initialization value.
+    Throws an exception if rows or columns are less than or equal 2.
+
+    @param rows : the number of rows in the matrix
+    @param cols : the number of columns in the matrix
+    *********************************************/
+    if (rows <= ROWS || cols <= COLS) {
+        throw std::invalid_argument("Matrix dimensions must be at least 2x2.");
+    }
+    this->rows = rows;
+    this->cols = cols;
+    this->sorted = false;
+
+    data = new int*[rows];
+
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new int[cols];
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            data[i][j] = INITVALUE;
+        }
+    }
+}
+
+Matrix::Matrix(int rows, int cols, int initValue) {
+
+    /*********************************************
+    Overloaded constructor to initialize the Matrix with given
+    rows and columns, and an optional initialization value.
+    Throws an exception if rows or columns are less than or equal 2.
+
+    @param rows : the number of rows in the matrix
+    @param cols : the number of columns in the matrix
+    @param initValue : the initial value for all elements
+    *********************************************/
+    if (rows <= ROWS || cols <= COLS) {
+        throw std::invalid_argument("Matrix dimensions must be at least 2x2.");
+    }
+    this->rows = rows;
+    this->cols = cols;
+    this->sorted = false;
+
+    data = new int*[rows];
+
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new int[cols];
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            data[i][j] = initValue;
+        }
+    }
+}
+
+
