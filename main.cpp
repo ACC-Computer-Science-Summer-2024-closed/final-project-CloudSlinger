bf412fdf3a355c0e5166f4982194cbb84bac6bd3
CloudSlinger <tom.tang@g.austincc.edu>
2024-07-04 10:37:47 -0500

    Updated console output to main() function to test objects.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,41 @@ a function to print a message to the console.
 
 #include "main.h"
 
+
 int main() {
+
+    cout << "Attempting to set Matrix: matrix(3, 3, 5)" << endl;
+    try {
+        Matrix matrix(3, 3, 5);
+
+        cout << "Matrix:" << endl;
+        matrix.printMatrix();
+
+        cout << "Attempting to set Row: index 2" << endl;
+        try {
+            int rowIndex = 2;
+            cout << "Row:" << endl;
+            matrix.printRow(rowIndex);
+
+        } catch (int e) {
+            cout << "Invalid row index." << endl;
+        }
+
+        cout << "Attempting to set Column: index 1" << endl;
+        try {
+            int colIndex = 1;
+            cout << "Column:" << endl;
+            matrix.printColumn(colIndex);
+
+        } catch (int e) {
+            cout << "Invalid column index." << endl;
+        }
+
+    } catch (int e) {
+        cout << "Row/columns must be larger than 2." << endl;
+    }
+
+
 
     return 0;
 }
