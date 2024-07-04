c854b1ff39d177683da1605b92d39155473457e2
CloudSlinger <tom.tang@g.austincc.edu>
2024-07-04 14:07:16 -0500

    Updated console output to main() function to test setElement() method.

--- main.cpp
+++ main.cpp
@@ -10,77 +10,80 @@ a function to print a message to the console.
 
 int main() {
 
-    cout << "Attempting to create and initialize matrix(2, 2)" << endl;
+    cout << "Attempting to create and initialize matrix(2, 2)..." << endl;
     try {
         Matrix matrix(2, 2);
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 2)" << endl;
+    cout << "Attempting to create and initialize matrix(3, 2)..." << endl;
     try {
         Matrix matrix(3, 2);
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(2, 3)" << endl;
+    cout << "Attempting to create and initialize matrix(2, 3)..." << endl;
     try {
         Matrix matrix(2, 3);
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 3)" << endl;
+    cout << "Attempting to create and initialize matrix(3, 3)..." << endl;
     try {
         Matrix matrix(3, 3);
+        cout << "Matrix initialized successfully." << endl;
         matrix.printMatrix();
         cout << endl;
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix dimensions must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(2, 2, 0)" << endl;
+    cout << "Attempting to create and initialize matrix(2, 2, 0)..." << endl;
     try {
         Matrix matrix(2, 2, 0);
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix dimensions must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 2, 0)" << endl;
+    cout << "Attempting to create and initialize matrix(3, 2, 0)..." << endl;
     try {
         Matrix matrix(3, 2, 0);
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(2, 3, 0)" << endl;
+    cout << "Attempting to create and initialize matrix(2, 3, 0)..." << endl;
     try {
         Matrix matrix(2, 3, 0);
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 3, 0)" << endl;
+    cout << "Attempting to create and initialize matrix(3, 3, 0)..." << endl;
     try {
         Matrix matrix(3, 3, 0);
+        cout << "Matrix initialized successfully." << endl;
         matrix.printMatrix();
         cout << endl;
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 3, 5)" << endl;
+    cout << "Attempting to create and initialize matrix(3, 3, 5)..." << endl;
     try {
         Matrix matrix(3, 3, 5);
+        cout << "Matrix initialized successfully." << endl;
         matrix.printMatrix();
         cout << endl;
         int rowIndex = 1;
@@ -94,11 +97,12 @@ int main() {
         int newRowIndex = 1;
         int newColIndex = 1;
         matrix.setElement(newRowIndex, newColIndex, newValue);
-        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << ":" << endl;
+        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
         matrix.printMatrix();
         cout  << endl;
         cout << "Row index " << rowIndex << ":" << endl;
         matrix.printRow(rowIndex);
+        cout << endl;
         cout << "Sum: " << matrix.rowSum(rowIndex) << endl;
         cout << "Average: " << matrix.rowAverage(rowIndex) << endl;
         cout << "Min: " << matrix.min() << endl;
@@ -110,29 +114,64 @@ int main() {
         newColIndex = 1;
         rowIndex = 2;
         matrix.setElement(newRowIndex, newColIndex, newValue);
-        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << ":" << endl;
+        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
         matrix.printMatrix();
         cout  << endl;
         cout << "Row index " << rowIndex << ":" << endl;
         matrix.printRow(rowIndex);
+        cout << endl;
         cout << "Sum: " << matrix.rowSum(rowIndex) << endl;
         cout << "Average: " << matrix.rowAverage(rowIndex) << endl;
         cout << "Min: " << matrix.min() << endl;
         cout << "Max: " << matrix.max() << endl;
         cout << endl;
 
+        newValue = 19;
+        newRowIndex = 0;
+        newColIndex = 0;
+        matrix.setElement(newRowIndex, newColIndex, newValue);
+        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
 
+        newValue = 32;
+        newRowIndex = 0;
+        newColIndex = 1;
+        matrix.setElement(newRowIndex, newColIndex, newValue);
+        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
 
+        newValue = 2;
+        newRowIndex = 0;
+        newColIndex = 2;
+        matrix.setElement(newRowIndex, newColIndex, newValue);
+        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
 
+        rowIndex = 0;
+        matrix.printMatrix();
+        cout  << endl;
+        cout << "Row index " << rowIndex << ":" << endl;
+        matrix.printRow(rowIndex);
+        cout << endl;
+        cout << "Sum: " << matrix.rowSum(rowIndex) << endl;
+        cout << "Average: " << matrix.rowAverage(rowIndex) << endl;
+        cout << "Min: " << matrix.min() << endl;
+        cout << "Max: " << matrix.max() << endl;
+        cout << endl;
 
-
-        int colIndex = 1;
+        int colIndex = 0;
+        cout << "Column index " << colIndex << ":" << endl;
+        matrix.printColumn(colIndex);
+        colIndex = 1;
+        cout << endl;
+        cout << "Column index " << colIndex << ":" << endl;
+        matrix.printColumn(colIndex);
+        colIndex = 2;
+        cout << endl;
         cout << "Column index " << colIndex << ":" << endl;
         matrix.printColumn(colIndex);
         cout << endl;
 
+
     } catch (int e) {
-        cout << "Matrix row/columns must be larger than 2." << endl;
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
     }
 
 
