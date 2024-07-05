fd4903925d04a0ad92bdfeaa52c862ee5440cdbf
CloudSlinger <tom.tang@g.austincc.edu>
2024-07-05 10:30:05 -0500

    Updated console output to main() function to test sortArray() method.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@ a function to print a message to the console.
 
 #include "main.h"
 
-
 int main() {
 
-    cout << "Attempting to create and initialize matrix(2, 0)..." << endl;
+    cout << "Attempting to create and initialize matrix(2, 0)." << endl;
     try {
         Matrix matrix(2, 0);
     } catch (int e) {
@@ -18,7 +17,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(0, 2)..." << endl;
+    cout << "Attempting to create and initialize matrix(0, 2)." << endl;
     try {
         Matrix matrix(0, 2);
     } catch (int e) {
@@ -26,7 +25,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, -1)..." << endl;
+    cout << "Attempting to create and initialize matrix(3, -1)." << endl;
     try {
         Matrix matrix(3, -1);
     } catch (int e) {
@@ -34,7 +33,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(-1, 3)..." << endl;
+    cout << "Attempting to create and initialize matrix(-1, 3)." << endl;
     try {
         Matrix matrix(-1, 3);
     } catch (int e) {
@@ -42,7 +41,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(2, 2)..." << endl;
+    cout << "Attempting to create and initialize matrix(2, 2)." << endl;
     try {
         Matrix matrix(2, 2);
     } catch (int e) {
@@ -50,7 +49,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 2)..." << endl;
+    cout << "Attempting to create and initialize matrix(3, 2)." << endl;
     try {
         Matrix matrix(3, 2);
     } catch (int e) {
@@ -58,7 +57,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(2, 3)..." << endl;
+    cout << "Attempting to create and initialize matrix(2, 3)." << endl;
     try {
         Matrix matrix(2, 3);
     } catch (int e) {
@@ -66,18 +65,25 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 3)..." << endl;
+    cout << "Attempting to create and initialize matrix(3, 3)." << endl;
     try {
         Matrix matrix(3, 3);
         cout << "Matrix initialized successfully." << endl;
         matrix.printMatrix();
-        cout << endl;
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+
     } catch (int e) {
         cout << "Error: Matrix dimensions must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(2, 2, 0)..." << endl;
+    cout << "Attempting to create and initialize matrix(2, 2, 0)." << endl;
     try {
         Matrix matrix(2, 2, 0);
     } catch (int e) {
@@ -85,7 +91,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, -1, 0)..." << endl;
+    cout << "Attempting to create and initialize matrix(3, -1, 0)." << endl;
     try {
         Matrix matrix(3, -1, 0);
     } catch (int e) {
@@ -93,7 +99,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(-1, 3, 0)..." << endl;
+    cout << "Attempting to create and initialize matrix(-1, 3, 0)." << endl;
     try {
         Matrix matrix(-1, 3, 0);
     } catch (int e) {
@@ -101,7 +107,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 2, 0)..." << endl;
+    cout << "Attempting to create and initialize matrix(3, 2, 0)." << endl;
     try {
         Matrix matrix(3, 2, 0);
     } catch (int e) {
@@ -109,7 +115,7 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(2, 3, 0)..." << endl;
+    cout << "Attempting to create and initialize matrix(2, 3, 0)." << endl;
     try {
         Matrix matrix(2, 3, 0);
     } catch (int e) {
@@ -117,34 +123,53 @@ int main() {
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 3, -1)..." << endl;
+    cout << "Attempting to create and initialize matrix(3, 3, -1)." << endl;
     try {
         Matrix matrix(3, 3, -1);
         cout << "Matrix initialized successfully." << endl;
         matrix.printMatrix();
-        cout << endl;
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
     } catch (int e) {
         cout << "Error: Matrix row/columns must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 3, 0)..." << endl;
+    cout << "Attempting to create and initialize matrix(3, 3, 0)." << endl;
     try {
         Matrix matrix(3, 3, 0);
         cout << "Matrix initialized successfully." << endl;
         matrix.printMatrix();
-        cout << endl;
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
     } catch (int e) {
         cout << "Error: Matrix row/columns must be larger than 2." << endl;
         cout << endl;
     }
 
-    cout << "Attempting to create and initialize matrix(3, 3, 5)..." << endl;
+    cout << "Attempting to create and initialize matrix(3, 3, 5)." << endl;
     try {
         Matrix matrix(3, 3, 5);
         cout << "Matrix initialized successfully." << endl;
         matrix.printMatrix();
-        cout << endl;
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+
         int rowIndex = 1;
         cout << "Sum row index " << rowIndex << ": " << matrix.rowSum(rowIndex) << endl;
         cout << "Average row index " << rowIndex << ": " << matrix.rowAverage(rowIndex) << endl;
@@ -158,7 +183,13 @@ int main() {
         matrix.setElement(newRowIndex, newColIndex, newValue);
         cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
         matrix.printMatrix();
-        cout  << endl;
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
         cout << "Row index " << rowIndex << ":" << endl;
         matrix.printRow(rowIndex);
         cout << endl;
@@ -175,7 +206,13 @@ int main() {
         matrix.setElement(newRowIndex, newColIndex, newValue);
         cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
         matrix.printMatrix();
-        cout  << endl;
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
         cout << "Row index " << rowIndex << ":" << endl;
         matrix.printRow(rowIndex);
         cout << endl;
@@ -203,9 +240,33 @@ int main() {
         matrix.setElement(newRowIndex, newColIndex, newValue);
         cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
 
+        newValue = 28;
+        newRowIndex = 1;
+        newColIndex = 1;
+        matrix.setElement(newRowIndex, newColIndex, newValue);
+        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
+
+        newValue = 62;
+        newRowIndex = 1;
+        newColIndex = 0;
+        matrix.setElement(newRowIndex, newColIndex, newValue);
+        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
+
+        newValue = 4;
+        newRowIndex = 2;
+        newColIndex = 2;
+        matrix.setElement(newRowIndex, newColIndex, newValue);
+        cout << "Setting element at index (" << newRowIndex << ", " << newColIndex << ") to " << newValue << "." << endl;
+
         rowIndex = 0;
         matrix.printMatrix();
-        cout  << endl;
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
         cout << "Row index " << rowIndex << ":" << endl;
         matrix.printRow(rowIndex);
         cout << endl;
@@ -228,12 +289,153 @@ int main() {
         matrix.printColumn(colIndex);
         cout << endl;
 
+        cout << "Rotate Matrix so rows become columns and columns become rows." << endl;
+        matrix.rotateMatrix();
+        matrix.printMatrix();
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+
+        rowIndex = 0;
+        cout << "Row index " << rowIndex << ":" << endl;
+        matrix.printRow(rowIndex);
+        rowIndex = 1;
+        cout << endl;
+        cout << "Row index " << rowIndex << ":" << endl;
+        matrix.printRow(rowIndex);
+        rowIndex = 2;
+        cout << endl;
+        cout << "Row index " << rowIndex << ":" << endl;
+        matrix.printRow(rowIndex);
+        cout << endl;
+
+        int findValue1 = 17;
+        int findValue2 = 100;
+        int findValue3 = 28;
+        cout << "Looking for values " << findValue1 << ", " << findValue2 << ", " << findValue3 << " in matrix." << endl;
+        matrix.printMatrix();
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+        if (matrix.findValue(findValue1)) {
+            cout << "Found: " << findValue1 << endl;
+        } else {
+            cout << "Not found: " << findValue1 << endl;
+        }
+        if (matrix.findValue(findValue2)) {
+            cout << "Found " << findValue2 << endl;
+        } else {
+            cout << "Not found: " << findValue2 << endl;
+        }
+        if (matrix.findValue(findValue3)) {
+            cout << "Found " << findValue3 << endl;
+        } else {
+            cout << "Not found: " << findValue3 << endl;
+        }
+        cout << endl;
+
+        cout << "Matrix before sorting each row." << endl;
+        matrix.printMatrix();
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+
+        matrix.sortMatrix();
+
+        cout << "Matrix after sorting each row." << endl;
+        matrix.printMatrix();
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+
+        findValue1 = -3;
+        findValue2 = 2;
+        findValue3 = 56;
+        cout << "Binary search for values " << findValue1 << ", " << findValue2 << ", " << findValue3 << " in matrix." << endl;
+        if (matrix.findValue(findValue1)) {
+            cout << "Found: " << findValue1 << endl;
+        } else {
+            cout << "Not found: " << findValue1 << endl;
+        }
+        if (matrix.findValue(findValue2)) {
+            cout << "Found " << findValue2 << endl;
+        } else {
+            cout << "Not found: " << findValue2 << endl;
+        }
+        if (matrix.findValue(findValue3)) {
+            cout << "Found " << findValue3 << endl;
+        } else {
+            cout << "Not found: " << findValue3 << endl;
+        }
+        cout << endl;
 
     } catch (int e) {
         cout << "Error: Matrix row/columns must be larger than 2." << endl;
     }
 
+    cout << "Attempting to create and initialize a new matrix(10, 10, 1)." << endl;
+    try {
+        Matrix matrix(10, 10, 1);
+        cout << "Matrix initialized successfully." << endl;
+        matrix.printMatrix();
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+    } catch (int e) {
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
+    }
 
+    cout << "Attempting to create and initialize a new matrix(8, 5, 2)." << endl;
+    try {
+        Matrix matrix(8, 5, 2);
+        cout << "Matrix initialized successfully." << endl;
+        matrix.printMatrix();
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+    } catch (int e) {
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
+    }
+
+    cout << "Attempting to create and initialize a new matrix(6, 10, 3)." << endl;
+    try {
+        Matrix matrix(6, 10, 3);
+        cout << "Matrix initialized successfully." << endl;
+        matrix.printMatrix();
+        cout << "Sorted: ";
+        if (matrix.getSorted()) {
+            cout << "true";
+        } else {
+            cout << "false";
+        }
+        cout << "\n" << endl;
+    } catch (int e) {
+        cout << "Error: Matrix row/columns must be larger than 2." << endl;
+    }
 
     return 0;
 }
